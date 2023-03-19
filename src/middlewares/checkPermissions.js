60d92e23d4c8c12ef9cea2e59df7b1649ee1a846
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-03-19 15:30:06 +0100

    update: checkPermissions middleware, errors file, validators

--- src/middlewares/checkPermissions.js
+++ src/middlewares/checkPermissions.js
@@ -1,11 +1,76 @@
 import jsonwebtoken from "jsonwebtoken"
 import config from "../config.js"
+import PermissionsModel from "../db/models/Permissions.js"
+import UserModel from "../db/models/UserModel.js"
+import { InvalidAccessError } from "../errors.js"
 
-const checkPermissions = (req, res, next) => {
+const getPermByMethod = (method) => {
+  let perm = ""
+
+  switch (method) {
+    case "GET": {
+      perm = "R"
+
+      break
+    }
+
+    case "post": {
+      perm = "C"
+
+      break
+    }
+      
+    case "patch": {
+      perm = "U"
+
+      break
+    }
+      
+    case "delete": {
+      perm = "D"
+
+      break
+    } 
+  }
+
+  return perm
+}
+
+const checkPermissions = async(req, res, next) => {
   const jwt = req.headers.authorization?.slice(7)
 
   try {
-    console.log("jwt :", jwt)
+    const { payload } = jsonwebtoken.verify(jwt, config.security.jwt.secret)
+    console.log("jwt :", payload)
+
+    const user = await UserModel.query()
+      .select("id", "role")
+      .where("id", payload.user.id)
+    
+    const perms = await PermissionsModel.query()
+      .select("permission_key", "permission_value")
+      .where("roleId", user.role)
+      .groupBy("permission_key", "permission_value")
+    
+    const permissions = perms.reduce((acc, { permission_key, permission_value }) => {
+      acc[permission_key] = permission_value
+
+      return acc
+    }, {})
+
+    const method = req.method
+    const ressource = req.url.trim().split("/")[1]
+    const permissionNeeded = getPermByMethod(method)
+
+    // console.log("method : ", method)
+    // console.log("ressource :", ressource)
+    // console.log("perms needed :", permissionNeeded)
+    // console.log("ressource checked : ", rolePermissions[ressource])
+    // console.log("authorized ? : ", rolePermissions[ressource].includes(permissionNeeded))
+
+    if (!permissions[ressource].includes(permissionNeeded)) {
+      throw new InvalidAccessError
+    }
 
     next()
   } catch (err) {
--- src/validators.js
+++ src/validators.js
@@ -11,11 +11,11 @@ export const titleValidator = yup.string().min(1).max(300)
 export const contentValidator = yup.string().min(1)
 
 // users
-export const displayNameValidator = yup.string().min(1)
-
+export const lastNameValidator = yup.string().min(1)
+export const firstNameValidator = yup.string().min(1)
 export const emailValidator = yup.string().email()
-
 export const passwordValidator = yup.string().min(8)
+export const roleValidator = yup.number()
 
 // collection (pagination, order, etc.)
 export const limitValidator = yup.number().integer().min(1).max(100).default(5)
